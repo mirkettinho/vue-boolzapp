28b69b10799a1626d3f7d1eee7d542cefb05b2df
mirkettinho <mirkodifranco30@gmail.com>
2023-04-18 12:45:46 +0200

    [add] contacts card

--- milestone-1/assets/js/script.js
+++ milestone-1/assets/js/script.js
@@ -1,9 +1,50 @@
-const {createApp} = vue;
+const {createApp} = Vue;
 
 createApp({
   data(){
     return{
-      
+      contacts:[
+        {
+          name: "Michele",
+          avatar: "./assets/img/avatar_1.jpg",
+          visible: true,
+        },
+        {
+          name: "Fabio",
+          avatar: "./assets/img/avatar_2.jpg",
+          visible: true,
+        },
+        {
+          name: "Samuele",
+          avatar: "./assets/img/avatar_3.jpg",
+          visible: true,
+        },
+        {
+          name: "Alessandro B.",
+          avatar: "./assets/img/avatar_4.jpg",
+          visible: true,
+        },
+        {
+          name: "Alessandro L.",
+          avatar: "./assets/img/avatar_5.jpg",
+          visible: true,
+        },
+        {
+          name: "Claudia",
+          avatar: "./assets/img/avatar_6.jpg",
+          visible: true,
+        },
+        {
+          name: "Federico",
+          avatar: "./assets/img/avatar_7.jpg",
+          visible: true,
+        },
+        {
+          name: "Davide",
+          avatar: "./assets/img/avatar_8.jpg",
+          visible: true,
+        }
+      ]
     }
   }
-}).mount(#app)
+}).mount("#app")
